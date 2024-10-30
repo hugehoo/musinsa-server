48822ad4c5183bb84a66a39ae8cdf7873eabf2d7
hoo <tbnsok40@gmail.com>
2024-10-30 09:27:19 +0900

    feat: resource package

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,3 +22,81 @@ INSERT INTO products (brand_id, category, price) VALUES
                                                      (2, 'CAP', 2000),
                                                      (2, 'SOCKS', 2000),
                                                      (2, 'ACCESSORY', 2200);
+
+-- Insert products for brand C
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (3, 'TOP', 10000),
+                                                     (3, 'OUTER', 6200),
+                                                     (3, 'PANTS', 3300),
+                                                     (3, 'SNEAKERS', 9200),
+                                                     (3, 'BAG', 2200),
+                                                     (3, 'CAP', 1900),
+                                                     (3, 'SOCKS', 2200),
+                                                     (3, 'ACCESSORY', 2100);
+
+-- Insert products for brand D
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (4, 'TOP', 10100),
+                                                     (4, 'TOP', 5000),
+                                                     (4, 'OUTER', 5100),
+                                                     (4, 'PANTS', 3000),
+                                                     (4, 'SNEAKERS', 9500),
+                                                     (4, 'BAG', 2500),
+                                                     (4, 'CAP', 1500),
+                                                     (4, 'SOCKS', 2400),
+                                                     (4, 'ACCESSORY', 2000);
+
+-- Insert products for brand E
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (5, 'TOP', 10700),
+                                                     (5, 'OUTER', 5000),
+                                                     (5, 'PANTS', 3800),
+                                                     (5, 'SNEAKERS', 9900),
+                                                     (5, 'BAG', 2300),
+                                                     (5, 'CAP', 1800),
+                                                     (5, 'SOCKS', 2100),
+                                                     (5, 'ACCESSORY', 2100);
+
+-- Insert products for brand F
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (6, 'TOP', 11200),
+                                                     (6, 'OUTER', 7200),
+                                                     (6, 'PANTS', 4000),
+                                                     (6, 'SNEAKERS', 9300),
+                                                     (6, 'BAG', 2100),
+                                                     (6, 'CAP', 1600),
+                                                     (6, 'SOCKS', 2300),
+                                                     (6, 'ACCESSORY', 1900);
+
+-- Insert products for brand G
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (7, 'TOP', 10500),
+                                                     (7, 'OUTER', 5800),
+                                                     (7, 'PANTS', 3900),
+                                                     (7, 'SNEAKERS', 9000),
+                                                     (7, 'BAG', 2200),
+                                                     (7, 'CAP', 1700),
+                                                     (7, 'SOCKS', 2100),
+                                                     (7, 'ACCESSORY', 2000);
+
+-- Insert products for brand H
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (8, 'TOP', 10800),
+                                                     (8, 'OUTER', 6300),
+                                                     (8, 'PANTS', 3100),
+                                                     (8, 'SNEAKERS', 9700),
+                                                     (8, 'BAG', 2100),
+                                                     (8, 'CAP', 1600),
+                                                     (8, 'SOCKS', 2000),
+                                                     (8, 'ACCESSORY', 2000);
+
+-- Insert products for brand I
+INSERT INTO products (brand_id, category, price) VALUES
+                                                     (9, 'TOP', 11400),
+                                                     (9, 'OUTER', 6700),
+                                                     (9, 'PANTS', 3200),
+                                                     (9, 'SNEAKERS', 9500),
+                                                     (9, 'BAG', 2400),
+                                                     (9, 'CAP', 1700),
+                                                     (9, 'SOCKS', 1700),
+                                                     (9, 'ACCESSORY', 2400);
